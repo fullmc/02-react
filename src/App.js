784e1502f19cd073aa5b3d91d0c19321ef979d47
MC <marie-claire.lambo@edu.devinci.fr>
2024-07-26 10:40:24 +0200

    API endpoint and env work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+	const redirectUrl = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+	const endpoint = process.env.REACT_APP_SPOTIFY_ENDPOINT;
+	const responseType = "token";
+
+	return (
+		<div className="App">
+			<h1>API Endpoint</h1>
+			<a
+				href={`${endpoint}?client_id=${clientSecret}&redirect_uri=${redirectUrl}&response_type=${responseType}`}>
+				API endpoint
+			</a>
+		</div>
+	);
 }
 
 export default App;
